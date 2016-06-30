8583e34b7f57cd95cc9a827b03fd9a7a02f7c83b
Katherine De La Cruz <kdelacruz@rcp.pe>
2016-06-30 16:18:49 -0500

    Add modules

--- public/app.js
+++ public/app.js
@@ -1145,16 +1145,6 @@ process.umask = function() { return 0; };
 },{}],5:[function(require,module,exports){
 var page = require('page');
 
-var main = document.getElementById('main-container');
-
-page('/', function (ctx, next) {
-  main.innerHTML = 'HOME <a href="/signup">Signup</a>';
-});
-
-page('/signup', function (ctx, next) {
-  main.innerHTML = 'SIGNUP <a href="/">home</a>';
-});
-
 page.start();
 
 },{"page":2}]},{},[5]);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 var page = require('page');
 
-var main = document.getElementById('main-container')
+require('./homepage');
+require('./signup');
 
-page('/', function (ctx, next) {
-  main.innerHTML = 'HOME <a href="/signup">Signup</a>';
-})
-
-page('/signup', function (ctx, next) {
-  main.innerHTML = 'SIGNUP <a href="/">home</a>';
-})
-
-page.start();
+page();
