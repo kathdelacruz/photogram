15ce284535bd02d4a55af01eca0b07fdd5a6137a
Katherine De La Cruz <kdelacruz@rcp.pe>
2016-06-30 11:42:48 -0500

    Add pagination with pagejs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-var numeros = [ 400, 200, 1, -23 ];
+var page = require('page');
 
- var numerosMas1= numeros.map(n => n + 1)
+var main = document.getElementById('main-container')
 
-console.log(numerosMas1);
+page('/', function (ctx, next) {
+  main.innerHTML = 'HOME <a href="/signup">Signup</a>';
+})
+
+page('/signup', function (ctx, next) {
+  main.innerHTML = 'SIGNUP <a href="/">home</a>';
+})
+
+page.start();
